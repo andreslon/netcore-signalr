d01e33d5508cf4994ce4bc3e2b86f5b8a3eea377
Andrés Londoño <andreslon@outlook.com>
2018-12-04 13:37:15 -0500

    new structure

--- signalr.events/wwwroot/js/chat.js
+++ signalr.events/wwwroot/js/chat.js
@@ -1,9 +1,7 @@
 "use strict";
-
+//zone.js which seems to corrupt the WebSocket APIs, this is the solution
 Object.defineProperty(WebSocket, 'OPEN', { value: 1, });
-
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-
 connection.on("ReceiveMessage", function(message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = msg;
@@ -11,11 +9,9 @@ connection.on("ReceiveMessage", function(message) {
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
 });
-
 connection.start().catch(function(err) {
     return console.error(err.toString());
 });
-
 document.getElementById("sendButton").addEventListener("click", function(event) {
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", message).catch(function(err) {
